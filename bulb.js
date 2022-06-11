c407639f7f5fbf3642f0c068ae9d92076d529e9d
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2022-06-10 21:34:38 -0500

    Create bulb.js

---
+++ bulb.js
@@ -0,0 +1,23 @@
+function bulbAI(){
+	var master = getSummoner()
+
+	// We move towards him
+	moveToward(master);
+
+	useChip(CHIP_HELMET, master);
+	if(getLife(master) < getTotalLife(master)){
+		useChip(CHIP_BANDAGE, master);
+	}
+	useChip(CHIP_PROTEIN, master);
+	var enemy = getNearestEnemy();
+	if(canUseChip(CHIP_PEBBLE, enemy)){
+		useChip(CHIP_PEBBLE, enemy);
+	}
+	if(getLife(master) < 150){
+		// self-sacrifice lol
+		moveToward(enemy)
+	}else {
+		moveAwayFrom(enemy)
+	}
+	
+}
