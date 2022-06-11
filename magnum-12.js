0832f336e5736df8556679feac986d39137a1964
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2022-06-10 21:33:44 -0500

    Update magnum-12.js

--- magnum-12.js
+++ magnum-12.js
@@ -4,24 +4,27 @@
  * To learn the LeekScript language: leekwars.com/help/tutorial
  * To learn more about the available functions: leekwars.com/help/documentation
 **/
+include('bulb')
 
 global lastdist
 
 var ME = getEntity()
-var ENGAGE_RANGE = getWeaponMaxRange(WEAPON_MAGNUM)+7 // 7 chosen b/c likely sum(moves) > 7 while enemy move likely < 7
+var ENGAGE_RANGE = getWeaponMaxRange(WEAPON_MAGNUM)+6 // 6 chosen b/c likely sum(moves) > 6 while enemy move likely < 6
 if(!getWeapon(ME)) {
 setWeapon(WEAPON_MAGNUM); // initial weapon
 }
 
-
+var pathlen = null
 var enemy = getNearestEnemy();
 var dist = getCellDistance(getCell(), getCell(enemy))
-/*
-if(canUseChip(CHIP_PUNY_BULB, enemy)) {
-	summon(CHIP_PUNY_BULB, enemy, )
-}*/
+pathlen = getPathLength(getCell(), getCell(enemy))
 
-if(canUseChip(CHIP_CURE, ME) and getCooldown(CHIP_CURE, ME) == 0 and getLife(ME) < getTotalLife(ME) * 0.7 and dist + getMP() > getWeaponMaxRange(getWeapon(enemy)) + getMP(enemy)){
+// always summon lol
+summon(CHIP_PUNY_BULB, getCell(ME) - 1, bulbAI)
+
+var canSafeRetreat = canUseChip(CHIP_CURE, ME) and getCooldown(CHIP_CURE, ME) == 0 and getLife(ME) < getTotalLife(ME) * 0.7 and dist + getMP() > getWeaponMaxRange(getWeapon(enemy)) + getMP(enemy)
+
+if(canSafeRetreat or getLife(ME) < 100 and getLife(enemy) > 250){
 	debug('retreating')
 	useChip(CHIP_KNOWLEDGE, ME)
 	useChip(CHIP_CURE, ME)
@@ -29,8 +32,13 @@ if(canUseChip(CHIP_CURE, ME) and getCooldown(CHIP_CURE, ME) == 0 and getLife(ME)
 	return;
 }
 
+if(getWisdom(ME) > 100 and getLife(ME) < getTotalLife(ME) * 0.8){
+	// obv has a buff, probably from last turn
+	useChip(CHIP_CURE, ME)
+}
+
 // move towards enemy
-while(getMP(ME) > 0 and getPathLength(getCell(ME), getCell(enemy)) > 1){ // prevent inf loops
+while(getMP(ME) > 0 and getTurn() > 1){ // prevent inf loops
 	enemy = getNearestEnemy();
 	var mindist = getPathLength(getCell(ME), getCell(enemy))
 
@@ -60,19 +68,25 @@ if(!canUseWeapon(enemy) and getWeapon() == WEAPON_LASER) {
 	}
 }
 dist = getCellDistance(getCell(), getCell(enemy))
+pathlen = getPathLength(getCell(), getCell(enemy))
 
-if(!canUseWeapon(enemy) and dist <= ENGAGE_RANGE + 5) {
+if(!canUseWeapon(enemy) and pathlen <= ENGAGE_RANGE + 4) {
 	// 3-turn cooldown chips
 	useChip(CHIP_SOLIDIFICATION, ME)
 	useChip(CHIP_SHIELD, ME)
 }
 
-if(!canUseWeapon(enemy) and dist <= ENGAGE_RANGE) {
-	useChip(CHIP_MOTIVATION, ME)
-	useChip(CHIP_PROTEIN, ME)
-	useChip(CHIP_HELMET, ME)
+if(dist <= ENGAGE_RANGE){
+	var isFullHealth = getLife() == getTotalLife()
+	// don't go into first fight without buff
+	if(!canUseWeapon(enemy) 
+	   or canUseChip(CHIP_MOTIVATION, ME) and isFullHealth
+	  ) {
+		useChip(CHIP_MOTIVATION, ME)
+		useChip(CHIP_PROTEIN, ME)
+		useChip(CHIP_HELMET, ME)
+	}
 }
-
 if(canUseChip(CHIP_STALACTITE, enemy)) { 
 	useChip(CHIP_STALACTITE, enemy) // highest damage
 } 
@@ -162,6 +176,11 @@ if(canUseChip(CHIP_SPARK, enemy)) {
 		useChip(CHIP_SPARK, enemy)
 	}
 
+}
+if(canUseChip(CHIP_KNOWLEDGE, ME)){
+	// always use knowledge before cure
+	// usually happens here when killed an enemy in one hit, still got TP left
+	useChip(CHIP_KNOWLEDGE, ME)
 }
 if(canUseChip(CHIP_CURE, ME) and getLife(ME) < getTotalLife(ME)){
 	useChip(CHIP_CURE, ME)
